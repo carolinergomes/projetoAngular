8142e792ddb0672c3b21fbe29bcb8b4dd6b76066
carolinergomes <caroline.gomes@estudante.ifms.edu.br>
2022-09-21 15:34:47 -0300

    heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,13 +7,33 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  constructor() { }
   hero: Hero = {
     id: 1,
     name: 'Thor'
   };
-  constructor() { }
+  selectedHero?: Hero
+
+  heroes: Hero[] = [
+    {id : 1, name: 'Mulher Maravilha'},
+    {id : 2, name: 'Viuva Negra'},
+    {id : 3, name: 'Capitã Marvel'},
+    {id : 4, name: 'Tempestade'},
+    {id : 5, name: 'Jean Grey'}
+
+];
+selectHero(hero : Hero):void{
+this.selectedHero = hero
+
+}
+
 
   ngOnInit(): void {
   }
 
 }
+
+
+//criar novo componente (criar obj aluno - id, nome, end,idade,nota1, nota2,media,status)
+//criar no componente (aluno) uma lista de 10 alunos
+// rederizar se o aluno esta aprovado ou repro
